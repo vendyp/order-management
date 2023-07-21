7cd9fc5b0ef8dd0412a1216ee61f585c77246bdd
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-07-21 18:06:03 +0700

    add new api getallscope

---
+++ src/order-management-api/src/OrderManagementApi.WebApi/Endpoints/UserManagement/GetAllScope.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OrderManagementApi.WebApi.Common;
+using OrderManagementApi.WebApi.Endpoints.UserManagement.Scopes;
+using OrderManagementApi.WebApi.Scopes;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace OrderManagementApi.WebApi.Endpoints.UserManagement;
+
+public class GetAllScope : BaseEndpoint<List<string>>
+{
+    private readonly IScopeManager _scopeManager;
+
+    public GetAllScope(IScopeManager scopeManager)
+    {
+        _scopeManager = scopeManager;
+    }
+
+    [HttpGet("users/scopes")]
+    [Authorize]
+    [RequiredScope(typeof(UserManagementScope), typeof(UserManagementScopeReadOnly))]
+    [SwaggerOperation(
+        Summary = "Get all scope API",
+        Description = "",
+        OperationId = "UserManagement.GetAllScope",
+        Tags = new[] { "UserManagement" })
+    ]
+    [ProducesResponseType(typeof(List<string>), StatusCodes.Status200OK)]
+    public override Task<ActionResult<List<string>>> HandleAsync(CancellationToken cancellationToken = new())
+    {
+        return Task.FromResult<ActionResult<List<string>>>(_scopeManager.GetAllScopes());
+    }
+}
