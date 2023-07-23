86a86e0ea7aadc4babd8d91cc6bc7329b3688ac4
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-07-23 15:58:36 +0700

    add new api get product by id

---
+++ src/order-management-api/src/OrderManagementApi.WebApi/Dto/ProductManagementDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagementApi.WebApi.Dto;
+
+public record ProductManagementDetailDto : ProductManagementDto
+{
+    public string? File { get; set; }
+    public string? CreatedByName { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? LastUpdatedByName { get; set; }
+    public DateTime? LastUpdatedAt { get; set; }
+}
---
+++ src/order-management-api/src/OrderManagementApi.WebApi/Endpoints/ProductManagement/GetProductById.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderManagementApi.Domain.Entities;
+using OrderManagementApi.Shared.Abstractions.Databases;
+using OrderManagementApi.Shared.Abstractions.Models;
+using OrderManagementApi.WebApi.Common;
+using OrderManagementApi.WebApi.Dto;
+using OrderManagementApi.WebApi.Endpoints.ProductManagement.Scopes;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace OrderManagementApi.WebApi.Endpoints.ProductManagement;
+
+public class GetProductById : BaseEndpoint<GetProductByIdRequest, ProductManagementDetailDto>
+{
+    private readonly IDbContext _dbContext;
+
+    public GetProductById(IDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("products/{productId}")]
+    [Authorize]
+    [RequiredScope(typeof(ProductManagementScope))]
+    [SwaggerOperation(
+        Summary = "Get Product by Id",
+        Description = "",
+        OperationId = "ProductManagement.GetProductById",
+        Tags = new[] { "ProductManagement" })
+    ]
+    [ProducesResponseType(typeof(ProductManagementDetailDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Error), StatusCodes.Status400BadRequest)]
+    public override async Task<ActionResult<ProductManagementDetailDto>> HandleAsync(
+        [FromRoute] GetProductByIdRequest request, CancellationToken cancellationToken = new())
+    {
+        var product = await _dbContext.Set<Product>()
+            .Where(e => e.ProductId == request.ProductId)
+            .FirstOrDefaultAsync(cancellationToken);
+        if (product is null)
+            return BadRequest(Error.Create("Product not found"));
+
+        var dto = new ProductManagementDetailDto
+        {
+            ProductId = product.ProductId,
+            Name = product.Name,
+            Description = product.Description,
+            Price = product.Price,
+            File = product.Image,
+            CreatedAt = product.CreatedAt,
+            CreatedByName = product.CreatedByName,
+            LastUpdatedAt = product.LastUpdatedAt,
+            LastUpdatedByName = product.LastUpdatedByName
+        };
+
+        return dto;
+    }
+}
